246390bbdf4d5456979d2987e280ac9537009dc7
PinkPanther-ny <alvincny529@gmail.com>
2023-08-25 00:36:26 +0800

    More fc

--- model.py
+++ model.py
@@ -13,13 +13,15 @@ class GazeNet(nn.Module):
         self.backbone.fc = nn.Identity()  # Remove the final classification layer
         self.fc1 = nn.Linear(512, 256)
         self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 2)
+        self.fc3 = nn.Linear(128, 32)
+        self.fc4 = nn.Linear(32, 2)
 
     def forward(self, x):
         x = self.backbone(x)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         x = self.fc3(x)
+        x = self.fc4(x)
         return x
 
 
