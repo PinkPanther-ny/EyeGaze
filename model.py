3cde3391101da88a7fd4813f6cda0897f5e48fa3
PinkPanther-ny <alvincny529@gmail.com>
2023-08-28 07:17:35 +0800

    Export onnx

--- model.py
+++ model.py
@@ -31,6 +31,8 @@ class GazeNet(nn.Module):
 if __name__ == '__main__':
     # Instantiate the model
     model = GazeNet().to('cuda')
+    model.eval()
+    print(model)
     batch = 16
     dummy_input = torch.rand((batch, 3, 224, 224)).to('cuda')
 
@@ -40,3 +42,15 @@ if __name__ == '__main__':
     # Verify the output shape is (batch, 2)
     assert output.shape == (batch, 2)
     print("Output shape:", output.shape)  # Output shape: (batch, 2)
+
+    import torch.onnx
+
+    # Export the model
+    torch.onnx.export(model,  # model being run
+                      dummy_input,  # model input (or a tuple for multiple inputs)
+                      "gaze_net.onnx",  # where to save the model (can be a file or file-like object)
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=11,  # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['input'],  # the model's input names
+                      output_names=['output'])  # the model's output names
