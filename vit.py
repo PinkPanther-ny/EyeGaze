c2b718234649abb8b735e2ae1ef0e0125aa49a1f
PinkPanther-ny <alvincny529@gmail.com>
2024-06-17 16:38:32 +0800

    Add bn and dropout

--- vit.py
+++ vit.py
@@ -3,22 +3,31 @@ import torch.onnx
 import torchvision.models
 from torchvision.models import ViT_B_16_Weights
 
+
 # Define the GazeNet model
 class GazeNet(nn.Module):
     def __init__(self):
         super(GazeNet, self).__init__()
-        self.backbone = torchvision.models.vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
+        self.backbone = torchvision.models.vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1)
         self.backbone.heads = nn.Identity()  # Remove the final classification layer
 
         self.head = nn.Sequential(
             nn.Linear(768, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(32, 2)
         )
 
