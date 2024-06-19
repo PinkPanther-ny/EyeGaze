37ce2d9a7863c1e8f6928d958830cc455a53c37f
PinkPanther-ny <alvincny529@gmail.com>
2024-06-19 18:30:17 +0800

    Add GazeNetL

--- vit.py
+++ vit.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
-import torch.onnx
 import torchvision.models
-from torchvision.models import ViT_B_16_Weights
+from torchvision.models import ViT_B_16_Weights, ViT_L_16_Weights
 
 
 # Define the GazeNet model
@@ -33,5 +32,40 @@ class GazeNet(nn.Module):
 
     def forward(self, x):
         x = self.backbone(x)
+        print(x.shape)
+        x = self.head(x)
+        return x
+
+
+# Define the GazeNet model
+class GazeNetL(nn.Module):
+    def __init__(self):
+        super(GazeNetL, self).__init__()
+        self.backbone = torchvision.models.vit_l_16(weights=ViT_L_16_Weights.IMAGENET1K_SWAG_E2E_V1)
+        self.backbone.heads = nn.Identity()  # Remove the final classification layer
+
+        self.head = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            # nn.Dropout(0.2),
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(32, 2)
+        )
+
+    def forward(self, x):
+        x = self.backbone(x)
+        print(x.shape)
         x = self.head(x)
         return x
