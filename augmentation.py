522a79d35b20efb68112d551da5b6d813d049e24
PinkPanther-ny <alvincny529@gmail.com>
2024-06-22 04:11:24 +0800

    New 50k normalization param

--- augmentation.py
+++ augmentation.py
@@ -23,7 +23,7 @@ train_aug = Compose(
         RandomGamma(gamma_limit=(80, 120), p=0.5),
         HueSaturationValue(hue_shift_limit=5, sat_shift_limit=20, val_shift_limit=10, p=0.5),
         CLAHE(p=0.5, clip_limit=2.0),
-        Normalize(p=1.0, mean=(0.50108877, 0.47534386, 0.47604029), std=(0.25453935, 0.25334834, 0.25508117)),
+        Normalize(p=1.0, mean=(0.45610908, 0.45873095, 0.48192639), std=(0.25258335, 0.241994, 0.23515741)),
         ToTensorV2()
     ]
 )
@@ -32,7 +32,7 @@ val_aug = Compose(
     [
         Resize(TARGET_SIZE, int(TARGET_SIZE / ORIGINAL_SIZE[0] * ORIGINAL_SIZE[1])),
         CenterCrop(TARGET_SIZE, TARGET_SIZE),
-        Normalize(p=1.0, mean=(0.50108877, 0.47534386, 0.47604029), std=(0.25453935, 0.25334834, 0.25508117)),
+        Normalize(p=1.0, mean=(0.45610908, 0.45873095, 0.48192639), std=(0.25258335, 0.241994, 0.23515741)),
         ToTensorV2()
     ]
 )
